d7807cee9f9b96ab056fe6194a262e83d21a1649
shKim <40208176+shKim-33@users.noreply.github.com>
2026-09-19 19:56:23 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -1,9 +1,29 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-static void Recursive(const vector<int>& _number, int& _answer, int idx = 0, int count = 0, int sum = 0)
+static int Solve_01(const vector<int>& _number)
+{
+    int _answer = 0;
+
+    for (int i = 0; i < _number.size(); ++i)
+    {
+        for (int j = i + 1; j < _number.size(); ++j)
+        {
+            for (int k = j + 1; k < _number.size(); ++k)
+            {
+                if (_number[i] + _number[j] + _number[k] == 0)
+                    _answer++;
+            }
+        }
+    }
+
+    return _answer;
+}
+
+static void Solve_02(const vector<int>& _number, int& _answer, int idx, int count, int sum)
 {
     if (count == 3)
     {
@@ -14,13 +34,54 @@ static void Recursive(const vector<int>& _number, int& _answer, int idx = 0, int
     if (idx == _number.size())
         return;
 
-    Recursive(_number, _answer, idx + 1, count + 1, sum + _number[idx]);
-    Recursive(_number, _answer, idx + 1, count, sum);
+    Solve_02(_number, _answer, idx + 1, count + 1, sum + _number[idx]);
+    Solve_02(_number, _answer, idx + 1, count, sum);
+}
+
+static int Solve_02_01(const vector<int>& _number, int idx, int count, int sum)
+{
+    if (count == 3)
+        return sum == 0;
+
+    if (idx == _number.size())
+        return 0;
+
+    return Solve_02_01(_number, idx + 1, count + 1, sum + _number[idx]) +
+           Solve_02_01(_number, idx + 1, count, sum);
+}
+
+static int Solve_03(const vector<int>& _number)
+{
+    int _answer = 0;
+    vector<int> check(_number.size(), 0);
+    fill(check.end() - 3, check.end(), 1);
+
+    do
+    {
+        int sum = 0;
+
+        for (int i = 0; i < _number.size(); ++i)
+        {
+            if (check[i] == 1)
+                sum += _number[i];
+        }
+
+        if (sum == 0)
+            _answer++;
+    }
+    while (next_permutation(check.begin(), check.end()));
+
+    return _answer;
 }
 
 int solution(vector<int> number)
 {
     int answer = 0;
-    Recursive(number, answer);
+
+    //answer = Solve_01(number);
+    //Solve_02(number,answer, 0, 0, 0);
+    //answer = Solve_02_01(number, 0, 0, 0);
+    answer = Solve_03(number);
+
     return answer;
 }
